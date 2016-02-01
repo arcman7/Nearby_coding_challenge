d2dd78a345c51361e99108b3da83ca22a9130ac9
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2016-01-31 17:49:07 -0800

    allow cors for any origin - accepts: post & get

--- Messages_json_API/app/controllers/application_controller.rb
+++ Messages_json_API/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  headers['Access-Control-Allow-Origin'] = '*'
+  headers['Access-Control-Allow-Methods'] = 'POST, GET'
+
   protect_from_forgery with: :exception
 end
