f1af8731ae3fa94c0940872c727dbf6d94ad3440
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2016-01-31 18:07:35 -0800

    create & show methods

--- Messages_json_API/app/controllers/messages_controller.rb
+++ Messages_json_API/app/controllers/messages_controller.rb
@@ -1,3 +1,16 @@
 class MessagesController < ApplicationController
+  def create
+    new_message = Message.create(message_params)
+  end
+
+  def show
+    @message = Message.find(params[:id])
+  end
+
+  private
+
+  def message_params
+      params.permit(:id, :threadID,:labelIds,:snippet, :historyId, :internalDate, :payload, :sizeEstimate)
+  end
 
 end
